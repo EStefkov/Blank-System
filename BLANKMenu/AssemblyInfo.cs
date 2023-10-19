26a235500567996546386f1e8fed2e50b9848d20
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2023-10-19 21:38:34 +0300

    Added comments
    Added comments for every function

--- BLANKMenu/AssemblyInfo.cs
+++ BLANKMenu/AssemblyInfo.cs
@@ -8,3 +8,13 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+
+ /* 
+        MVVM is an architectural pattern commonly used in software development,
+        particularly when creating applications for mobile devices and web applications. Here's a brief explanation:
+      1.Model: Represents the data and business logic of your application. This component contains the information that the application uses and processes.
+       2.View: Represents the user interface of the application. It's what the user sees and interacts with.
+       3.ViewModel: This component acts as an intermediary between the model and the view. The ViewModel contains the logic for presenting and 
+        processing data from the model to provide it to the view. Additionally, the ViewModel can accept input data from the view and convey it to the model.
+        */
